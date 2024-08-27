e88726e713d576e88e0e95f8f6a16620a50884ba
Hryhorii_Struk <136825012+hryhoriistruk@users.noreply.github.com>
2024-08-27 08:29:32 +0300

    Update test.txt

--- src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) {
-        Running running = new Running(args);
-        running.run();
-    }
-}
--- src/main/java/ua/prom/roboticsdmc/util/FileUtility.java
+++ src/main/java/ua/prom/roboticsdmc/util/FileUtility.java
@@ -1,4 +1,4 @@
-package util;
+package ua.prom.roboticsdmc.util;
 
 import java.io.IOException;
 import java.nio.file.Files;
